5af73c97a2dc6688eb53a897e9c9ec8f4034f282
Gregory Andersson <grandersson1991@gmail.com>
2022-08-18 17:23:33 -0400

    demo changes

--- src/Components/LoginPanel.js
+++ src/Components/LoginPanel.js
@@ -4,11 +4,11 @@ import { ethers } from 'ethers'
 import { Fragment, useState, useEffect } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { XIcon } from '@heroicons/react/outline'
-const contractAddress = '0xdf3a0252D5c75640380ab29b2a55b7534Aa0De5B'
-const url = process.env.REACT_APP_ALCHEMY_RINKEBY_URL;
-const provider = new ethers.providers.JsonRpcProvider(url);
-const privateKey = process.env.REACT_APP_RINKEBY_PRIVATE_KEY;
-const wallet = new ethers.Wallet(privateKey, provider);
+const contractAddress = '0xdf3a0252D5c75640380ab29b2a55b7534Aa0De5B';
+const demoUrl = 'https://eth-rinkeby.alchemyapi.io/v2/2O0Sv9b_YJgqPnuyj0ynbT_Wz76lOZAn';
+const provider = new ethers.providers.JsonRpcProvider(demoUrl);
+const demoPrivateKey = '1cd4c4adff9cd49fe1b23624525c59ed0580a9a1020d5782ac2105943610436e';
+const wallet = new ethers.Wallet(demoPrivateKey, provider);
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
--- src/functions/ipfs.js
+++ src/functions/ipfs.js
@@ -2,9 +2,9 @@ import { create } from 'ipfs-http-client';
 import { Buffer } from 'buffer';
 import axios from 'axios';
 
-const projectId = '2CDb5s8hxGZEZgQtUPxjk9KwDtH';
-const projectSecret = 'fa39105826d2be41836e84e8a989a16a';
-const buffer = Buffer.from(projectId + ":" + projectSecret);
+const demoProjectId = '2CDb5s8hxGZEZgQtUPxjk9KwDtH';
+const demoProjectSecret = 'fa39105826d2be41836e84e8a989a16a';
+const buffer = Buffer.from(demoProjectId + ":" + demoProjectSecret);
 const bufferString = buffer.toString('base64');
 const auth = 'Basic ' + bufferString;
 const client = create({
